57d9cba3bf955f375909e31fdca0a84baeea7817
Simion Ion <52240502+simionion@users.noreply.github.com>
2022-02-02 23:34:49 +0200

    bugfix

--- src/circle/modal.cljs
+++ src/circle/modal.cljs
@@ -20,18 +20,19 @@
 
 (defn modal
   []
-  (let [circle (re-frame/subscribe [:active-circle])]
+  (let [circle @(re-frame/subscribe [:active-circle])]
+    ^{:key circle}
     [:div.modal
      {:style {:display (when @shown? "block")}}
      [:div.modal-content
       [:span.close {:title "Close" :on-click modal-hide} "×"]
-      [:p (gstring/format "Adjust diameter of circle at (%d, %d)" (:x @circle) (:y @circle))]
+      [:p (gstring/format "Adjust diameter of circle at (%d, %d)" (:x circle) (:y circle))]
       [:input {:type         "range"
                :name         "diameter"
                :min          1
                :max          100
-               :defaultValue (:d @circle)
+               :defaultValue (:d circle nil)
                :on-change    (fn [event]
-                               (re-frame/dispatch [:active-circle-changed @circle (-> event .-target .-value)]))}]]]))
+                               (re-frame/dispatch [:active-circle-changed circle (-> event .-target .-value)]))}]]]))
 
 
